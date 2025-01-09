0d193d375f4cf2f6f0fd2038b2b9adbbfa0383ef
maisasugandh <msugandh02@gmail.com>
2025-01-09 15:15:49 +0530

    new data

---
+++ data.sql
@@ -0,0 +1,2 @@
+insert into movie_details(movie_title,movie_language,movie_release_date,movie_image_url) values ('Pokiri','Telugu','2024-08-09','');
+insert into movie_details(movie_title,movie_language,movie_release_date,movie_image_url) values ('Business man','Telugu','2024-08-10','');
