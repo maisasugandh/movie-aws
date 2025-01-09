fa42bee575078f9c8bfc6536572930cd0912c432
maisasugandh <118497024+maisasugandh@users.noreply.github.com>
2025-01-09 15:55:19 +0530

    Delete schema.sql

--- schema.sql
+++
@@ -1,7 +0,0 @@
-create table movie_details(
-movie_id int auto_increment primary key,
-movie_title varchar(20),
-movie_language varchar(20),
-movie_release_date DATE,
-movie_image_url varchar(200)
-);
